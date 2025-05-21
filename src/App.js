6c15e559388755789bcbbf6c6b00dbbfea97f7d7
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-05-21 01:59:23 +0100

    Fix: removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import { ThemeProvider, useTheme } from './contexts/ThemeContext';
 

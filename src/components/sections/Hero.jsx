143884c7cff53664aac64fd6e8aa1284a9e687b1
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-05-26 03:55:05 +0100

    Optimised image loading

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -19,7 +19,13 @@ const Hero = () => {
 
         {/* Moon */}
         <Parallax speed={moonSpeed} className="svg-item layer-moon">
-          <img src="/images/moonv2.svg" alt="Layer moon" />
+          <img 
+            src="/images/moonv2.svg" 
+            alt="Layer moon" 
+            width={336} 
+            height={336} 
+            loading="eager" // Prioritised load
+          />
           <h1>Benjamin<br />Sanders-Wyatt</h1>
         </Parallax>
 
@@ -27,15 +33,31 @@ const Hero = () => {
         {/* Mountains */}
 
         <Parallax speed={backSpeed} className="svg-item layer-mountain mountain-back">
-          <img src="/images/mountain/back-mid.svg" alt="Layer mountain" />
+            <img 
+              src="/images/mountain/back-mid.svg" 
+              alt="Layer mountain" 
+              width={1920} 
+              height={925} 
+            />
         </Parallax>
 
         <Parallax speed={midSpeed} className="svg-item layer-mountain mountain-middle">
-          <img src="/images/mountain/middle-mid.svg" alt="Layer mountain" />
+            <img 
+              src="/images/mountain/middle-mid.svg" 
+              alt="Layer mountain" 
+              width={1920} 
+              height={759} 
+            />
         </Parallax>
 
         <Parallax speed={frontSpeed} className="svg-item layer-mountain mountain-front">
-          <img src="/images/mountain/front-mid.svg" alt="Layer mountain" />
+          <img 
+            src="/images/mountain/front-mid.svg" 
+            alt="Layer mountain" 
+            width={1920} 
+            height={757} 
+            loading="eager" // Prioritised load
+          />
         </Parallax>
 
 

6fefbdb2164641eb43e2f3cd74b556c83eb73ee5
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-05-26 05:58:37 +0100

    Reworked moon parallax

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 
 import { Parallax } from 'react-scroll-parallax';
-import useParallaxSpeed from '../../hooks/parallaxSpeed';
 
 import './hero.css';
 
 
 const Hero = () => {
-  
-  const moonSpeed = useParallaxSpeed(-300);
-  const backSpeed = useParallaxSpeed(-10);
-  const midSpeed = useParallaxSpeed(-5);
-  const frontSpeed = useParallaxSpeed(0);
+
 
   return (
     <section id='hero' className='section hero-section'>
       <div className="hero-visuals">
 
         {/* Moon */}
-        <Parallax speed={moonSpeed} className="svg-item layer-moon">
+        <Parallax speed={-30} className="svg-item layer-moon">
           <img 
             src="/images/moonv2.svg" 
             alt="Layer moon" 
@@ -32,7 +27,7 @@ const Hero = () => {
 
         {/* Mountains */}
 
-        <Parallax speed={backSpeed} className="svg-item layer-mountain mountain-back">
+        <Parallax speed={-5} className="svg-item layer-mountain mountain-back">
             <img 
               src="/images/mountain/back-mid.svg" 
               alt="Layer mountain" 
@@ -41,7 +36,7 @@ const Hero = () => {
             />
         </Parallax>
 
-        <Parallax speed={midSpeed} className="svg-item layer-mountain mountain-middle">
+        <Parallax speed={0} className="svg-item layer-mountain mountain-middle">
             <img 
               src="/images/mountain/middle-mid.svg" 
               alt="Layer mountain" 
@@ -50,7 +45,7 @@ const Hero = () => {
             />
         </Parallax>
 
-        <Parallax speed={frontSpeed} className="svg-item layer-mountain mountain-front">
+        <Parallax speed={5} className="svg-item layer-mountain mountain-front">
           <img 
             src="/images/mountain/front-mid.svg" 
             alt="Layer mountain" 

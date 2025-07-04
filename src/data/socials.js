624916078d0820c7d9214b563743a41b7b9e29a3
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-07-04 22:52:49 +0100

    Added social information

---
+++ src/data/socials.js
@@ -0,0 +1,27 @@
+
+export const SOCIAL_LINKS = [
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/ben-sanders-wyatt',
+        iconFile: '/images/linkedin.svg',
+        width: 512,
+        height: 512,
+    },
+    {
+        name: 'GitHub',
+        url: 'https://github.com/benjaminsanderswyatt',
+        iconFile: '/images/github.svg',
+        width: 128,
+        height: 128,
+    },
+];
+
+
+export const EMAIL = 
+{
+    email: 'bensanderswyatt@gmail.com',
+    iconFile: '/images/mail.svg',
+    width: 24,
+    height: 24,
+
+};
